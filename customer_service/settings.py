1d134b28cac86bbec80559ccd202c5a806295062
Leksite <olalekannurudeen0@gmail.com>
2024-06-14 08:36:18 +0100

    new updates20

--- customer_service/settings.py
+++ customer_service/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mtn_bot'
+    'mtn_bot',
     'mtn_bot.apps.MtnBotConfig',
 ]
 
