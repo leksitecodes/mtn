410996d9ffe1035b182a877855a3fb706bd4408a
Leksite <olalekannurudeen0@gmail.com>
2024-06-14 01:30:02 +0100

    new updates16

--- mtn_bot/urls.py
+++ mtn_bot/urls.py
@@ -5,3 +5,6 @@ urlpatterns = [
     path('get_response/', views.get_response, name='get_response'),
     path('', views.chat_page, name='home'),  # Add this line to make chat page the home page
 ]
+
+
+
