6ef7fe2f7f5fb8da228eefa88a5ce83642d7106c
Leksite <olalekannurudeen0@gmail.com>
2024-06-14 01:23:52 +0100

    new updates15

--- mtn_bot/urls.py
+++ mtn_bot/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get/', views.get_response, name='get_response'),
+    path('get_response/', views.get_response, name='get_response'),
     path('', views.chat_page, name='home'),  # Add this line to make chat page the home page
 ]
