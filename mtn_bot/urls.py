19afe757a8409d50ba991be351cf6822303643b6
Leksite <olalekannurudeen0@gmail.com>
2024-06-14 01:36:52 +0100

    new updates17

--- mtn_bot/urls.py
+++ mtn_bot/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get_response/', views.get_response, name='get_response'),
+    path('get/', views.get_response, name='get_response'),
     path('', views.chat_page, name='home'),  # Add this line to make chat page the home page
 ]
 
--- mtn_bot/views.py
+++ mtn_bot/views.py
@@ -16,25 +16,38 @@
 # def chat_page(request):
 #     return render(request, 'chat.html')
 
+# from django.http import JsonResponse
+# from django.shortcuts import render
+
+# import logging
+
+# # Configure logging
+# logger = logging.getLogger(__name__)
+
+# # View function to handle user messages and return bot responses
+# def get_response(request):
+#     from .chatbot import bot
+#     user_text = request.GET.get('userMessage')
+#     logger.debug("Received user message: %s", user_text)
+    
+#     bot_response = str(bot.get_response(user_text))
+#     logger.debug("Bot response: %s", bot_response)
+    
+#     return JsonResponse({'response': bot_response})
+
+# def chat_page(request):
+#     logger.debug("Rendering chat page")
+#     return render(request, 'chat.html')
+
 from django.http import JsonResponse
 from django.shortcuts import render
 
-import logging
-
-# Configure logging
-logger = logging.getLogger(__name__)
-
-# View function to handle user messages and return bot responses
 def get_response(request):
-    from .chatbot import bot
-    user_text = request.GET.get('userMessage')
-    logger.debug("Received user message: %s", user_text)
-    
-    bot_response = str(bot.get_response(user_text))
-    logger.debug("Bot response: %s", bot_response)
-    
-    return JsonResponse({'response': bot_response})
+    # Log the incoming request
+    print("Received request:", request.GET)
+
+    # Return a static response for debugging purposes
+    return JsonResponse({'response': "This is a static response for debugging."})
 
 def chat_page(request):
-    logger.debug("Rendering chat page")
     return render(request, 'chat.html')
