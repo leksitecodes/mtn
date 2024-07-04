43ebcd3087609ce1c607bfa770a140a5308a8cd4
Leksite <olalekannurudeen0@gmail.com>
2024-07-04 10:19:47 +0100

    new updates33

--- customer_service/settings.py
+++ customer_service/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED HOSTS"," ").split(" ")
 
 # Application definition
 
